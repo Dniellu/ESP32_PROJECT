866c220b2bbaa08bb53e14bfd743d7ac30320a2e
Dniellu <144087986+Dniellu@users.noreply.github.com>
2025-09-10 20:09:13 +0800

    12345678

--- new/bus.js
+++ new/bus.js
@@ -22,6 +22,7 @@ function findNearestBUS() {
       card.className = "card"; // 套用 CSS
 
       card.innerHTML = `
+        <img src="img/bus.png" alt="${st.name}">
         <h3>${st.name}</h3>
         <p>📍 ${st.address}</p>
         <p>📏 ${st.distance.toFixed(2)} 公里</p>
